55774331bd746db92f143847af03f0a19fbdbe9b
Jonas Frei <freijo01@students.zhaw.ch>
2018-03-20 16:08:33 +0100

    Basic project

---
+++ RTM/setup_RTM.cpp
@@ -0,0 +1,10 @@
+// Setup the Feather M0 pinmodes etc..
+// 
+// 
+
+#include "setup_RTM.h"
+
+
+void set_RTM(void) {
+	return;
+}
---
+++ RTM/setup_RTM.h
@@ -0,0 +1,15 @@
+// setup_RTM.h
+
+#ifndef _SETUP_RTM_h
+#define _SETUP_RTM_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+void set_RTM(void);
+
+#endif
+
